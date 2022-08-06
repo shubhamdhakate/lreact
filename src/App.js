1278ecac5404b562d8066b05e8c9a19d7125b1fc
shubhamdhakate <97968232+shubhamdhakate@users.noreply.github.com>
2022-08-06 15:48:34 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
+import Article from './Components/Article';
+import { BlogProvider } from './Components/BlogContext';
+import CategoryPage from './Components/CategoryPage';
+import Header from './Components/Header';
+import Home from './Components/Home';
+import PageNotFound from './Components/PageNotFound';
+
+function App() {
+
+
+
+
+
+
+
+  return (
+<div >
+    <BlogProvider>
+      <Router>
+        <Header />
+        
+
+        <Routes>
+          <Route path='/' element={<Navigate to={'/home'} />} />
+          <Route path='/home' element={<Home />} />
+          <Route
+            path='/article/:articleId/:articleCategory'
+            element={<Article />}
+          />
+          <Route
+            path='/categoryPage/:categoryName'
+            element={<CategoryPage />}
+          />
+          <Route path='/*' element={<PageNotFound />} />
+        </Routes>
+      </Router>
+    </BlogProvider>
+ 
+    </div>
+  );
+}
+
+export default App;
